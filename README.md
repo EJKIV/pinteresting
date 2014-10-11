0acde6f9bfb976a08060ecf8eda6b5d9917cbbee
ejkiv <jim.kernan@gmail.com>
2014-10-11 11:38:18 -0400

    image reset

---
+++ README.md
@@ -0,0 +1,3 @@
+== README
+
+This is my second Ruby application.  My hope is that they become a bit easier as time progresses, so that I can actually build something.
---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,54 @@
+class PinsController < ApplicationController
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user?, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @pins = Pin.all
+  end
+
+  def show
+  end
+
+  def new
+    @pin = current_user.pins.build
+  end
+
+  def edit
+  end
+
+  def create
+   @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      flash[:success] = "Sweet. You have a new pin!"
+      redirect_to @pin
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    @pin.update(pin_params)
+  end
+
+  def destroy
+    @pin.destroy
+  end
+
+  private
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    def correct_user?
+      @pin = current_user.pins.find_by(id: params[:id])
+      if @pin.nil?
+        flash[:danger] = "You are not permitted to access this page"
+        redirect_to pins_path
+      end
+    end
+    
+    def pin_params
+      params.require(:pin).permit(:description, :image)
+    end
+end
